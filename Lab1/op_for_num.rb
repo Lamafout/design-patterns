f89da4491e4751e7f8ce598b2f27fe9684704b86
lamafout <dzordzvashington@gmail.com>
2024-09-07 19:44:03 +0300

    Task 2.2
    realised method for sum digits-devidor by 3

--- Lab1/op_for_num.rb
+++ Lab1/op_for_num.rb
@@ -1,4 +1,5 @@
 # вариант 2
+
 # task 2.1: реализовать метод подсчёта количества взаимно простых чисел
 def have_divider(num, begin_of_range)
   while begin_of_range != 0
@@ -18,3 +19,18 @@ end
 puts 'Введите число для для подсчёта взаимно простых чисел:'
 num = gets.to_i
 puts "Количество взаимно простых чисел: #{count_of_devidors(num)}"
+
+#task 2.2: сумма цифр числа, делящихся на 3
+
+def sum_of_digits(num)
+  sum = 0
+  while num!= 0
+    sum += num % 10 if num % 3 == 0
+    num /= 10
+  end
+  sum
+end
+
+puts 'Введите число для суммы цифр, делящихся на 3:'
+num = gets.to_i
+puts "Сумма цифр числа, делящихся на 3: #{sum_of_digits(num)}"

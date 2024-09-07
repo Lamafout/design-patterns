904bc156579b067e4e195df46faad551e5026b88
lamafout <dzordzvashington@gmail.com>
2024-09-07 20:28:47 +0300

    Task 2.3
    realised specific method and refactored code in op_for_num.rb

--- Lab1/op_for_num.rb
+++ Lab1/op_for_num.rb
@@ -1,26 +1,11 @@
 # вариант 2
 
-# task 2.1: реализовать метод подсчёта количества взаимно простых чисел
-# def have_divider(num, begin_of_range)
-#   while begin_of_range != 0
-#     num, begin_of_range = begin_of_range, num % begin_of_range
-#   end
-#   num
-# end
-
-# def count_of_deviders(num)
-#   count = 0
-#   (1...num).each do |i|
-#     count += 1 if have_divider(num, i) == 1
-#   end
-#   count
-# end
-
-# puts 'Введите число для для подсчёта взаимно простых чисел:'
-# num = gets.to_i
-# puts "Количество взаимно простых чисел: #{count_of_deviders(num)}"
-
-# task 2.2: сумма цифр числа, делящихся на 3
+def have_divider(num, begin_of_range)
+  while begin_of_range != 0
+    num, begin_of_range = begin_of_range, num % begin_of_range
+  end
+  num
+end
 
 def digits(num)
   digits = []
@@ -31,6 +16,16 @@ def digits(num)
   digits
 end
 
+#task 2.1: реализовать метод подсчёта количества взаимно простых чисел
+def count_of_deviders(num)
+  count = 0
+  (1...num).each do |i|
+    count += 1 if have_divider(num, i) == 1
+  end
+  count
+end
+
+# task 2.2: сумма цифр числа, делящихся на 3
 def sum_of_digits(num)
   sum = 0
   digits(num).each do |i|
@@ -39,11 +34,7 @@ def sum_of_digits(num)
   sum
 end
 
-puts 'Введите число для суммы цифр, делящихся на 3:'
-num = gets.to_i
-puts "Сумма цифр числа, делящихся на 3: #{sum_of_digits(num)}"
-
-task 2.3 
+#task 2.3 
 def specific_devider(num)
   max_count = 0
   result = 0
@@ -51,11 +42,27 @@ def specific_devider(num)
     if num % i == 0
       count = 0
       # пробегаемся по числу для подсчёта цифр, взаимно простых с текущим делителем
-      temp_num = num
-      while temp_num!= 0
-        count += 1 if have_divider
-    else
-      continue
+      digits(num).each do |j|
+        count += 1 if have_divider(i, j) == 1
+      end
+      if count >= max_count
+        max_count = count
+        result = i
+      end
     end
   end
+  result
 end
+
+# результаты
+puts 'Введите число для для подсчёта взаимно простых чисел:'
+num = gets.to_i
+puts "Количество взаимно простых чисел: #{count_of_deviders(num)}"
+
+puts 'Введите число для суммы цифр, делящихся на 3:'
+num = gets.to_i
+puts "Сумма цифр числа, делящихся на 3: #{sum_of_digits(num)}"
+
+puts 'Введите число:'
+num = gets.to_i
+puts "Делитель: #{specific_devider(num)}"

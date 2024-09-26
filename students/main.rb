d264c3c651d8446786e074c5105e71d1eda6f606
lamafout <dzordzvashington@gmail.com>
2024-09-26 20:48:35 +0300

    Added number validate

--- students/main.rb
+++ students/main.rb
@@ -1,8 +1,8 @@
 require_relative 'student_class'
 
-amal = Student.new('Blyagoz', 'Amal', 'Hazretovich', id: 1,  git: 'lamafout')
-sergey = Student.new('Lotarev', 'Sergey', 'Urievuch', id: 2, phone: '88005553535')
-mark = Student.new('Anikin', 'Mark', 'Andreevich', id: 3, email: 'mark.markovnik@gmail.com')
+amal = Student.new('Blyagoz', 'Amal', 'Hazretovich', git: 'lamafout')
+sergey = Student.new('Lotarev', 'Sergey', 'Urievuch', phone: '88005553535')
+mark = Student.new('Anikin', 'Mark', 'Andreevich', email: 'mark.markovnik@gmail.com')
 puts amal
 puts sergey
 puts mark
--- students/student_class.rb
+++ students/student_class.rb
@@ -1,18 +1,37 @@
 class Student
-  def initialize(surname, name, second_name, id: nil, phone: nil, telegram: nil, email: nil, git: nil)
+  #статическое поле для айди по умолчанию
+  @id_counter = 0
+  attr_accessor :id, :surname, :name, :second_name, :phone, :telegram, :email, :git
+  def initialize(surname, name, second_name, id: self.class.increment_id, phone: nil, telegram: nil, email: nil, git: nil)
     @surname = surname
     @name = name
     @second_name = second_name
     @id = id
-    @phone = phone
+    self.phone = phone
     @telegram = telegram
     @email = email
     @git = git
   end
 
+  def phone=(new_phone)
+    if new_phone.nil? || self.class.is_phone_valid?(new_phone)
+      @phone = new_phone
+    else 
+      raise ArgumentError, 'Invalid phone number'
+    end
+  end
+
   def to_s
-    #get info about student in ine string and return it
     "ID: #{@id}, Surname: #{@surname}, Name: #{@name}, Second Name: #{@second_name}, Phone: #{@phone}, Telegram: #{@telegram}, Email: #{@email}, Git: #{@git}"
   end
-  attr_accessor :id, :surname, :name, :second_name, :phone, :telegram, :email, :git
+
+  #статический метод для инкрементирования статического поля и передачи  результата в конструктор
+  def self.increment_id
+    @id_counter += 1
+  end
+
+  def self.is_phone_valid?(phone)
+    return false if phone.nil?
+    !!phone.match(/^\+?[0-9]{10,15}$/)
+  end
 end

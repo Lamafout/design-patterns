1ca5b22c322e0318137060be2f6508f7336e2fb0
lamafout <dzordzvashington@gmail.com>
2024-09-26 21:31:06 +0300

    Now git, email and tg also validate

--- students/main.rb
+++ students/main.rb
@@ -1,9 +1,8 @@
 require_relative 'student_class'
 
-amal = Student.new('Blyagoz', 'Amal', 'Hazretovich', git: 'lamafout')
+amal = Student.new('Blyagoz', 'Amal', 'Hazretovich', git: 'github.com/lamafout')
 sergey = Student.new('Lotarev', 'Sergey', 'Urievuch', phone: '88005553535')
 mark = Student.new('Anikin', 'Mark', 'Andreevich', email: 'mark.markovnik@gmail.com')
 puts amal
 puts sergey
 puts mark
-puts amal.name
--- students/student_class.rb
+++ students/student_class.rb
@@ -8,9 +8,9 @@ class Student
     @second_name = second_name
     @id = id
     self.phone = phone
-    @telegram = telegram
-    @email = email
-    @git = git
+    self.telegram = telegram
+    self.email = email
+    self.git = git
   end
 
   def phone=(new_phone)
@@ -19,6 +19,30 @@ class Student
     else 
       raise ArgumentError, 'Invalid phone number'
     end
+  end
+
+  def email=(new_email)
+    if new_email.nil? || self.class.is_email_valid?(new_email)
+      @email = new_email
+    else
+      raise ArgumentError, "Invalid email"
+    end
+  end
+
+  def telegram=(new_telegram)
+    if new_telegram.nil? || self.class.is_telegram_valid?(new_telegram)  
+      @telegram = new_telegram
+    else
+      raise ArgumentError, "Invalid telegram"
+    end
+  end
+
+  def git=(new_git)
+      if new_git.nil? || self.class.is_git_valid?(new_git)
+        @git = new_git
+      else
+        raise ArgumentError, "Invalid git link"
+      end
   end
 
   def to_s
@@ -33,5 +57,20 @@ class Student
   def self.is_phone_valid?(phone)
     return false if phone.nil?
     !!phone.match(/^\+?[0-9]{10,15}$/)
+  end
+
+  def self.is_email_valid?(email)
+    return false if email.nil?
+    !!email.match?(/^[\w+_.-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)
+  end
+
+  def self.is_telegram_valid?(telegram)
+    return false if telegram.nil?
+    !!telegram.match?(/@[a-zA-Z0-9_]{5,}$/)
+  end
+
+  def self.is_git_valid?(git)
+    return false if git.nil?
+    !!git.match?(/^(https?:\/\/)?(www\.)?github\.com\/[a-zA-Z0-9_-]+$/)
   end
 end

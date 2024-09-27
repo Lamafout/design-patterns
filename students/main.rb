be10dc543c337e2f83c3db75968f4c15a0bd0fbe
lamafout <dzordzvashington@gmail.com>
2024-09-27 16:56:46 +0300

    Added get_info method

--- students/main.rb
+++ students/main.rb
@@ -1,11 +1,9 @@
 require_relative 'student_class'
 
-amal = Student.new('Blyagoz', 'Amal', 'Hazretovich', git: 'github.com/lamafout', email: 'lamafout@yandex.ru')
+amal = Student.new('Blyagoz', 'Amal', 'Hazretovich', git: 'github.com/lamafout', email: 'lamafout@yandex.ru', telegram: '@lamafout')
 sergey = Student.new('Lotarev', 'Sergey', 'Urievuch', phone: '88005553535')
 mark = Student.new('Anikin', 'Mark', 'Andreevich', email: 'mark.markovnik@gmail.com')
 puts amal
 puts sergey
 puts mark
-puts amal.validate?
-amal.set_contacts(telegram: '@lamafout')
-puts amal
+puts amal.get_info
--- students/student_class.rb
+++ students/student_class.rb
@@ -11,6 +11,18 @@ class Student
     self.git = git
     set_contacts(phone: phone, telegram: telegram, email: email)
   end
+
+  def get_info()
+    contacts = {'phone': @phone, 'email': @email, 'telegram': @telegram}
+    contact = nil
+    contacts.each do |key, value|
+      if !value.nil?  
+        contact = "#{key}: #{value}"
+        break
+      end
+    end
+    "#{@surname} #{@name[0]}.#{@second_name[0]}. #{"Git: #{@git}, " if !@git.nil?}#{contact if !contact.nil?}"
+  end
   
   #метод-писатель для гита
   def git=(new_git)
@@ -35,22 +47,22 @@ class Student
   #ниже методы валидации строковых необязательных полей
   def self.is_phone_valid?(phone)
     return false if phone.nil?
-    !!phone.match(/^\+?[0-9]{10,15}$/)
+    phone.match?(/^\+?[0-9]{10,15}$/)
   end
   
   def self.is_email_valid?(email)
     return false if email.nil?
-    !!email.match?(/^[\w+_.-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)
+    email.match?(/^[\w+_.-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)
   end
   
   def self.is_telegram_valid?(telegram)
     return false if telegram.nil?
-    !!telegram.match?(/@[a-zA-Z0-9_]{5,}$/)
+    telegram.match?(/@[a-zA-Z0-9_]{5,}$/)
   end
   
   def self.is_git_valid?(git)
     return false if git.nil?
-    !!git.match?(/^(https?:\/\/)?(www\.)?github\.com\/[a-zA-Z0-9_-]+$/)
+    git.match?(/^(https?:\/\/)?(www\.)?github\.com\/[a-zA-Z0-9_-]+$/)
   end
   
   #переопределение to_s для простого поулчения информации об экземпляре
@@ -80,7 +92,7 @@ class Student
       raise ArgumentError, "Invalid email"
     end
   end
-
+  
   def telegram=(new_telegram)
     if new_telegram.nil? || self.class.is_telegram_valid?(new_telegram)  
       @telegram = new_telegram

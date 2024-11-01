79b7ed8ddfcf00a7e753e3a5bc471b1ff89926dc
lamafout <dzordzvashington@gmail.com>
2024-11-01 23:52:26 +0300

    L2T2.14

--- arrays/create_with_block/array_processor.rb
+++ arrays/create_with_block/array_processor.rb
@@ -13,4 +13,20 @@ class Array_processor
     result
   end
 
+  def flat_map
+    result = []
+    array.each do |elem|
+      if elem.is_a? Array
+        elem.each do |sub_elem|
+          sub_elem = yield elem
+        end
+        result.concat elem
+      else 
+        mapped_element = yield elem
+        result << mapped_element
+      end
+    end
+    result
+  end
+
 end

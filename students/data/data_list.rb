d3a3683272a626708f1856763a7c6acbe45dc45a
lamafout <dzordzvashington@gmail.com>
2024-11-15 22:44:57 +0300

    L4T6 & L4T7: refactored get_data and get_names methods
    Now get_data method is not abstract but it calls row_from_attrs method that must be implemented in the children

--- students/data/data_list.rb
+++ students/data/data_list.rb
@@ -1,6 +1,8 @@
 class Data_list
   def initialize(list)
     self.list = list
+    self.selected_elems = []
+    self.index = 0
   end
 
   def select(index)
@@ -9,11 +11,24 @@ class Data_list
   end
 
   def get_names
+    raise NotImplementedError, "Must be implemented!"
   end
 
   def get_data
+    result_data = []
+    self.selected_indexes.each do |index|
+      data = self.list[index]
+      new_row = [self.index] + row_from_attrs(data)
+      result_data << new_row 
+      self.index++
+    end
+    result_data
   end
-
+  
   private
-  attr_accessor :list, :selected_elems
+  attr_accessor :list, :selected_elems, :index
+
+  def row_from_attrs(data) # part of the code that must be implemented in children classes
+    raise NotImplementedError, "Must be implemented!"
+  end
 end
--- students/data/data_list_student_short.rb
+++ students/data/data_list_student_short.rb
@@ -2,21 +2,13 @@ require_relative 'data_list.rb'
 require_relative 'data_table.rb'
 
 class Data_list_student_short < Data_list
-  
-  def get_data
-    result_data = []
-    self.selected_indexes.each do |index|
-      data = self.list[index]
-      new_row = [self.index, data.initials, data.git, data.contact] #TODO: вынести основную логику в родительский класс?
-        result_data << new_row 
-    end
-    result_data
-  end
 
   def get_names
     ['initials', 'git', 'contact']
   end
 
   private
-  attr_accessor :index
+  def row_from_attrs(data)
+    return [data.initials, data.git, data.contact]
+  end
 end

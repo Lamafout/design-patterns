9e73aba649b7c140506f5d2e97e0f6b330fe4bbc
lamafout <dzordzvashington@gmail.com>
2024-11-15 21:05:46 +0300

    L4T1.2: added get_by_index method for data table class

--- students/data/data_table.rb
+++ students/data/data_table.rb
@@ -4,6 +4,16 @@ class Data_table
     self.data = data
   end
 
+  def get_by_index(row_index, column_index)
+    result = nil
+    if row_index >= 0 && row_index < data.length && column_index >= 0 && column_index < data[row_index].length
+      result = data[row_index][column_index]
+    else
+      raise IndexError, "Index is out of range"
+    end
+    result
+  end
+
   private
   attr_reader :data
 

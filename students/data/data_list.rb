1dbbc56ce0afe0c1630c80301015e6284dcea43d
lamafout <dzordzvashington@gmail.com>
2024-11-15 21:43:11 +0300

    L4T1.4: added select method and get_name and get_data abstract methods

--- students/data/data_list.rb
+++ students/data/data_list.rb
@@ -2,7 +2,17 @@ class Data_list
   def initialize(list)
     self.list = list
   end
-  
+
+  def select(index)
+    self.selected_elems = list[index]
+  end
+
+  def get_names
+  end
+
+  def get_data
+  end
+
   private
-  attr_accessor :list
+  attr_accessor :list, :selected_elems
 end

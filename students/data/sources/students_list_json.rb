4e427d17a08e7886f9f9417018df6fa6766be3bb
lamafout <dzordzvashington@gmail.com>
2024-11-16 01:36:58 +0300

    L4T2.1: added initialize method and refactored students list from json class

--- students/data/sources/students_list_json.rb
+++ students/data/sources/students_list_json.rb
@@ -3,27 +3,31 @@ require_relative '../../domain/entities/short_student_class.rb'
 require_relative '../../domain/entities/student_class.rb'
 
 class Student_list_JSON
-  def read_list_of_students(path)
-    file = File.open(path, 'r')
+  def initialize(path)
+    self.path = path
+  end
+
+  def read_list_of_students
+    file = File.open(self.path, 'r')
     
     list = JSON.parse(file)
     list.map do |object|
       Student.new(**object)
     end
-
-    self.students_list = list
   end
 
-  def write_list_of_students(path, list = self.student.list)
+  def write_list_of_students(list)
     data = list.map do |object|
       object.to_h
     end
-    File.open(path, 'w') do |file|
+    File.open(self.path, 'w') do |file|
       file.write(JSON.pretty_generate(data))
     end
   end
 
   def get_by_id(id)
+    students_list = read_list_of_students
+
     self.students_list.find do |student|
       student.id == id
     end
@@ -31,6 +35,8 @@ class Student_list_JSON
 
   def get_k_n_student_short_list(k = 1, n = 20)
     k = 1 if k < 1 
+    student_list = read_list_of_students
+
     if students_list[k * n]
       students_list[((k-1) * n)...(k*n)]
     else
@@ -38,33 +44,51 @@ class Student_list_JSON
   end
 
   def sort # sort by surname and name
+    students_list = read_list_of_students
+
     self.students_list.sort_by do |student|
       [student.surname, student.name]
     end
+
+    write_list_of_students(students_list)
   end
 
-  def input_student(student)
+  def insert_student(student)
+    students_list = read_list_of_students
+
     self.students_list.push(student)
+
+    write_list_of_students(students_list)
   end
 
   def replace_by_id(student)
+    students_list = read_list_of_students
+
     self.students_list.map do |object|
       if object.id == student.id
         object = student
       end
     end
+
+    write_list_of_students(students_list)
   end
 
   def delete_by_id(id)
+    students_list = read_list_of_students
+
     self.students_list.delete_if do |student|
       student.id == id
     end
+
+    write_list_of_students(students_list)
   end
 
   def get_student_short_count
+    students_list = read_list_of_students
+
     self.students_list.count
   end
 
   private
-  attr_accessor :students_list
+  attr_accessor :path
 end
--- students/main.rb
+++ students/main.rb
@@ -1,7 +1,8 @@
-require_relative 'domain/entities/student_class'
 require_relative 'domain/entities/short_student_class'
+require_relative 'domain/entities/student_class.rb'
 require_relative 'core/binary_tree/binary_tree.rb'
 require_relative 'data/model/data_list_student_short.rb'
+require_relative 'data/sources/students_list_json.rb'
 
 amal = Student.new('Blyagoz', 'Amal', 'Khazretovich', id: 123, git: 'github.com/lamafout', email: 'lamafout@yandex.ru', telegram: '@lamafout', birthdate: Date.new(2004, 6, 14))
 # sergey = Student.new('Lotarev', 'Sergey', 'Urievuch', phone: '88005553535', birthdate: Date.new(2004, 10, 26))
@@ -22,8 +23,16 @@ short_amal = Short_student.from_string(id: amal.id, string: amal.get_info)
 #   puts node.name 
 # end
 
-data_table = Data_table.new([[]])
-data_list = Data_list_student_short.new([short_amal])
-data_list.select(0)
-result =  data_list.get_data.get_by_index(0, 1)
-puts result
+# data_table = Data_table.new([[]])
+# data_list = Data_list_student_short.new([short_amal])
+# data_list.select(0)
+# result =  data_list.get_data.get_by_index(0, 1)
+# puts result
+
+sljson = Students_list_json.new('res/sudents_list.json')
+
+sljson.insert_student(amal)
+
+sljson.read_list_of_students.each do |student|
+  puts student.name
+end

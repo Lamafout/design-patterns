2023fe86e5ad049e483679c74502210483973a33
lamafout <dzordzvashington@gmail.com>
2024-11-29 23:33:09 +0300

    L4T2.2: created students list yaml class

--- students/main.rb
+++ students/main.rb
@@ -2,6 +2,7 @@ require_relative 'domain/entities/short_student_class'
 require_relative 'core/binary_tree/binary_tree.rb'
 require_relative 'data/model/data_list_student_short.rb'
 require_relative 'data/sources/students_list_json.rb'
+require_relative 'data/sources/student_list_yaml.rb'
 
 amal = Student.new(surname: 'Blyagoz', name: 'Amal', second_name: 'Khazretovich', git: 'github.com/lamafout', email: 'lamafout@yandex.ru', telegram: '@lamafout', birthdate: Date.new(2004, 6, 14))
 sergey = Student.new(surname: 'Lotarev', name: 'Sergey', second_name: 'Urievuch', phone: '88005553535', birthdate: Date.new(2004, 10, 26))
@@ -42,4 +43,12 @@ sljson = Student_list_JSON.new('res/data/sudents_list.json')
 
 sljson.sort.each do |element|
   puts element
+end
+
+slyaml = Student_list_YAML.new('res/data/students_list.yaml') 
+
+slyaml.insert_student(sergey)
+
+slyaml.read_list_of_students.each do |student|
+  puts student
 end

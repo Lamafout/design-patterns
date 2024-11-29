f2b8457dca0c8ca38ad1d7dcd78b21afb525927e
lamafout <dzordzvashington@gmail.com>
2024-11-29 23:46:59 +0300

    L4T3: created student list source class
    This class is a super class for other source classes (json and yaml)

--- students/data/sources/student_list_yaml.rb
+++ students/data/sources/student_list_yaml.rb
@@ -1,11 +1,9 @@
 require 'yaml'
 require_relative '../../domain/entities/short_student_class.rb'
 require_relative '../../domain/entities/student_class.rb'
+require_relative 'student_list_source.rb'
 
-class Student_list_YAML
-  def initialize(path)
-    self.path = path
-  end
+class Student_list_YAML < Student_list_source
 
   def read_list_of_students
     file = File.read(self.path)
@@ -23,66 +21,4 @@ class Student_list_YAML
       file.write(data.to_yaml)
     end
   end
-
-  def get_by_id(id)
-    students_list = read_list_of_students
-
-    students_list.find do |student|
-      student.id == id
-    end
-  end
-
-  def get_k_n_student_short_list(k = 1, n = 20)
-    k = 1 if k < 1
-    student_list = read_list_of_students
-
-    if student_list[k * n]
-      student_short_list = student_list[((k - 1) * n)...(k * n)].map do |student|
-        Short_student.from_student(student)
-      end
-    else
-      raise IndexError, 'Index out of range'
-    end
-  end
-
-  def sort # sort by surname and name
-    students_list = read_list_of_students.sort_by do |student|
-      student.initials
-    end
-  end
-
-  def insert_student(student)
-    students_list = read_list_of_students
-    students_list.push(student)
-
-    write_list_of_students(students_list)
-  end
-
-  def replace_by_id(student)
-    student_list = read_list_of_students.map do |object|
-      if object.id == student.id
-        object = student
-      end
-    end
-
-    write_list_of_students(student_list)
-  end
-
-  def delete_by_id(id)
-    student_list = read_list_of_students.delete_if do |student|
-      student.id == id
-    end
-
-    write_list_of_students(student_list)
-  end
-
-  def get_student_short_count
-    students_list = read_list_of_students
-
-    students_list.count
-  end
-
-  private
-
-  attr_accessor :path
 end

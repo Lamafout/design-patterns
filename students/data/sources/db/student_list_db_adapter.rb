652546406ddc4992627098ddbab701e1bfc53b2d
lamafout <dzordzvashington@gmail.com>
2024-12-06 23:27:11 +0300

    L4T4.2: Refactored
    Removed student list storage and db classes and repaced their logic to their adapters. Updated diagram

--- students/data/sources/db/student_list_db_adapter.rb
+++ students/data/sources/db/student_list_db_adapter.rb
@@ -1,43 +1,82 @@
 require_relative '../student_list_interface'
 require_relative './student_list_db'
+require_relative '../../model/db/pg_client.rb'
+require_relative '../../../domain/entities/student_class.rb'
+require_relative '../../../domain/entities/short_student_class.rb'
+
 class Student_list_db_adapter < Student_list_interface
+  attr_accessor :client 
+
   def initialize
-    self.student_list = Student_list_DB.new
-  end
-
-  def read_list_of_students
-    self.student_list.read_list_of_students
-  end
-
-  def write_list_of_students
-    self.student_list.write_list_of_students
+    self.client = PG_client.instance
   end
 
   def get_by_id(id)
-    self.student_list.get_by_id(id)
+    result = client.exec_params("SELECT * FROM students WHERE id = $1", [id])
+    raise "Student with id=#{id} not found" if result.ntuples.zero?
+
+    Student.from_hash(result[0]) 
   end
 
   def get_k_n_student_short_list(k = 1, n = 20)
-    self.student_list.get_k_n_student_short_list(k, n)
-  end
+    k = 1 if k < 1
+    offset = (k - 1) * n
 
-  def sort
-    self.student_list.sort
+    result = client.exec_params(
+      "SELECT id, surname, name, second_name FROM students ORDER BY id LIMIT $1 OFFSET $2",
+      [n, offset]
+    )
+
+    if result.ntuples.zero?
+      raise IndexError, 'Index out of range'
+    end
+
+    student_short_list = result.map do |row|
+      Short_student.from_student(Student.from_hash(row))
+    end
+
+    Data_list_student_short.new(student_short_list)
   end
 
   def insert_student(student)
-    self.student_list.insert_student(student)
+    client.exec_params(
+      "INSERT INTO students (surname, name, second_name, birthdate, phone, telegram, email, git) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)",
+      [
+        student.surname,
+        student.name,
+        student.second_name,
+        student.birthdate,
+        student.phone,
+        student.telegram,
+        student.email,
+        student.git
+      ]
+    )
   end
 
   def replace_by_id(student)
-    self.student_list.replace_by_id(student)
+    client.exec_params(
+      "UPDATE students SET surname = $1, name = $2, second_name = $3, birthdate = $4, phone = $5, telegram = $6, email = $7, git = $8 WHERE id = $9",
+      [
+        student.surname,
+        student.name,
+        student.second_name,
+        student.birthdate,
+        student.phone,
+        student.telegram,
+        student.email,
+        student.git,
+        student.id
+      ]
+    )
   end
 
   def delete_by_id(id)
-    self.student_list.delete_by_id(id)
+    client.exec_params("DELETE FROM students WHERE id = $1", [id])
   end
-  
+
   def get_student_short_count
-    self.student_list.get_student_short_count
+    result = client.exec("SELECT COUNT(*) FROM students")
+    result[0]['count'].to_i
   end
 end
--- students/data/sources/storage/student_list_storage_adapter.rb
+++ students/data/sources/storage/student_list_storage_adapter.rb
@@ -1,43 +1,120 @@
 require_relative '../student_list_interface.rb'
-require_relative 'student_list_storage.rb'
+require_relative '../../model/data_list_student_short.rb'
+require_relative 'source_strategy.rb'
 class Student_list_storage_adapter < Student_list_interface
-  def initialize(storage_strategy)
-    self.student_list = Student_list_storage.new(storage_strategy)
+  def initialize(strategy)
+    self.strategy = strategy
+  end
+
+  def strategy=(strategy)
+    if strategy.is_a?(Source_strategy)
+      @strategy = strategy
+    else 
+      raise TypeError, 'Wrong type of strategy'
+    end
   end
 
   def read_list_of_students
-    self.student_list.read_list_of_students
+    self.strategy.read_list_of_students
   end
 
   def write_list_of_students
-    self.student_list.write_list_of_students
+    self.strategy.write_list_of_students
   end
 
   def get_by_id(id)
-    self.student_list.get_by_id(id)
+    result = self.strategy.read_list_of_students.find do |student|
+      student.id == id
+    end
   end
 
   def get_k_n_student_short_list(k = 1, n = 20)
-    self.student_list.get_k_n_student_short_list(k, n)
+    k = 1 if k < 1 
+    student_list = self.strategy.read_list_of_students
+
+    if student_list[k * n]
+      student_short_list = student_list[((k-1) * n)...(k*n)].map do |student|
+        Short_student.from_student(student)
+      end
+      Data_list_student_short.new(student_short_list)
+    else
+      raise IndexError, 'Index out of range'
+    end
   end
 
-  def sort
-    self.student_list.sort
+  def sort # sort by surname and name
+    students_list = self.strategy.read_list_of_students.sort_by do |student|
+      student.initials
+    end
   end
 
   def insert_student(student)
-    self.student_list.insert_student(student)
+    students_list = self.strategy.read_list_of_students
+    if (unique_student?(student))
+      students_list.push(student)
+    else
+      raise ArgumentError, 'Student already exists'
+    end
+
+    self.strategy.write_list_of_students(students_list)
   end
 
   def replace_by_id(student)
-    self.student_list.replace_by_id(student)
+    student_list = self.strategy.read_list_of_students.map do |object|
+      if object.id == student.id
+        object = student
+      end
+    end
+    self.strategy.write_list_of_students(student_list)
   end
 
   def delete_by_id(id)
-    self.student_list.delete_by_id(id)
+    student_list = self.strategy.read_list_of_students.delete_if do |student|
+      student.id == id
+    end
+
+    self.strategy.write_list_of_students(student_list)
   end
   
   def get_student_short_count
-    self.student_list.get_student_short_count
+    students_list = self.strategy.read_list_of_students
+
+    students_list.count
+  end
+
+  private
+  def unique_student?(student)
+    unigue_git?(student.git) && unique_phone?(student.phone) && unique_email?(student.email) && unique_telegram?(student.telegram)
+  end
+
+  def unigue_git?(git)
+    unique_attr?(:git, git)
+  end
+
+  def unique_phone?(phone)
+    unique_attr?(:phone, phone)
+  end
+
+  def unique_email?(email)
+    unique_attr?(:email, email)
+  end
+
+  def unique_telegram?(telegram)
+    unique_attr?(:telegram, telegram)
+  end
+
+  def unique_attr?(symbol, value)
+    tree = Binary_tree.new
+    self.strategy.read_list_of_students.each do |student|
+      student.unique_indicator = symbol
+      tree.add(student)
+    end
+
+    tree.iterator.each do |student|
+      if student.unique_indicator == value
+        return false
+      end
+    end
+    true
   end
 end
